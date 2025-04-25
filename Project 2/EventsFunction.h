0a553010d91272c93155f138f2ac21ff500a8731
Josiah Galvan-Lewis <gta5everyda@gmail.com>
2025-04-24 19:37:57 -0600

    Create EventsFunction.h

---
+++ Project 2/EventsFunction.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib> 
+#include "Board.h"
+using namespace std;
+
+void eventRed(Board board){
+    cout<<"this tile is red"<<endl;
+}
+
+void eventGreen(Board board){
+    cout<<"this tile is green"<<endl;
+}
+
+void eventBlue(Board board){
+    cout<<"this tile is blue"<<endl;
+}
+
+void eventPurple(Board board){
+    cout<<"this tile is purple"<<endl;
+}
+
+void eventBrown(Board board){
+    cout<<"this tile is brown"<<endl;
+}
+
+void eventPink(Board board){
+    cout<<"this tile is pink"<<endl;
+}
+
+void eventOrange(Board board){
+    cout<<"this tile is orange"<<endl;
+}
+
+void eventGrey(Board board){
+    cout<<"this tile is grey"<<endl;
+}
