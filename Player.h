d174093db93b9731b9e22211d8e627bc7deda44a
Josiah Galvan-Lewis <gta5everyda@gmail.com>
2025-04-29 00:00:32 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,7 +1,6 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <iostream>
-#include <vector>
 #include "Board.h"
 
 using namespace std;
@@ -22,23 +21,30 @@ class Player {
         Player();
         int roll;
         string advisorNo;
+
         void storeStats(const vector<string>);
         void storePath(int);
         void storeAdvisor(string);
+
         void print();
         void setName(string);
         void setStrength(int);
+
         void setStamina(int);
         void setWisdom(int);
         void setPridePoints(int);
-        string getCharacterName();
-        int getStrength();
-        int getStamina();
-        int getWisdom();
+
         string getAdvisorName();
         string getAdvisorAbility();
         int getPath();
+
         string getPlayerName();
+        int getPridePoints();
+        int calculateTotal();
+
+        int getStrength();
+        int getWisdom();
+        int getStamina();
 };
 
 struct eventUpdate{
